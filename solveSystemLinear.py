d32663b307277e1c04a7b69272ad1970a22ae243
Isaac Choi <ichoi102@uottawa.ca>
2021-01-01 15:57:08 -0500

    change input methods and cleanup the code

--- solveSystemLinear.py
+++ solveSystemLinear.py
@@ -2,19 +2,21 @@ import numpy as np
 import pandas as pd
 
 
-# # we can specify a delimiter by putting ("filedir", delimiter = " ")
-df = pd.read_csv("~/Desktop/Coding/Python_Practice/SmallProject/input.csv")
+# a delimiter by putting ("filedir", delimiter = " ")
+df = pd.read_csv("./input.csv")
 
-# numpy array: single data type
-# python list: multiple data types
-
-# put a csv to a numpy array (then we can use the numpy library)
+# put a csv to a numpy array 
 input_matrix = df.to_numpy()  # arrays (2D, 3D)
 
-a = input_matrix[:-1]
-b = input_matrix[-1]
+# everything but the last column
+a = input_matrix[:,:-1]
+# last column
+b = input_matrix[:,-1]
 
-# print(a, b)
+# debug print
+print(a)
+print("--------")
+print(b)
 
 x = np.linalg.solve(a, b)
 print(x)
