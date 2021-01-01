34acfea3836f7ea54251fcb49703501e593dd54d
Isaac Choi <ichoi102@uottawa.ca>
2021-01-01 16:51:42 -0500

    added some print statements to help readability

--- solveSystemLinear.py
+++ solveSystemLinear.py
@@ -2,21 +2,39 @@ import numpy as np
 import pandas as pd
 
 
+def list_formator(input_list):
+    rtn_list = []
+
+    for each in input_list:
+        x = round(each, 3)
+        rtn_list.append(x)
+    return rtn_list
+
+
+# size = int(input("Dimension: ?"))
+
 # a delimiter by putting ("filedir", delimiter = " ")
 df = pd.read_csv("./input.csv")
 
-# put a csv to a numpy array 
+# put a csv to a numpy array
 input_matrix = df.to_numpy()  # arrays (2D, 3D)
 
+# (             |     )
+# (      A      |  B  )
+# (             |     )
+
 # everything but the last column
-a = input_matrix[:,:-1]
+a = input_matrix[:, :-1]
 # last column
-b = input_matrix[:,-1]
+b = input_matrix[:, -1]
 
 # debug print
-print(a)
-print("--------")
-print(b)
+print("The A matrix is: \n{}".format(a))
+print("-----------------")
+print("The B matrix is: \n{}".format(b))
 
 x = np.linalg.solve(a, b)
-print(x)
+
+print("Answers:")
+print(list(df.columns[:-1]))
+print(list_formator(x))
